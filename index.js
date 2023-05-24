322379936b88e2b0fa0ed5361dc348015290bba8
jlucasacosta <luacostadelgado@gmail.com>
2023-05-24 10:03:25 -0300

    New file Javascript

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const functionBuy = () => { 
-    alert('Compraste');
+const functionBuy = () => {
+  alert("Compraste");
+};
+
+const myCar = { make: "Ford", model: "Mustang", year: 1969 };
+
+const vestuario = {
+    vaquero: {
+        marca: "Levis",
+        color: "Negro"
+    }, 
+    championes: "Nike",
+    remera: "Adidas", 
+    gorro: "Lana"
 }
+
+const destinationChange = () => {
+    let title = document.querySelector('h1');
+
+    console.log("value title", title)
+
+    title.textContent = 'Nuevo valor visto en clase';
+
+    let test = document.querySelector('h6');
+
+    console.log("value test", test);
+}
+
